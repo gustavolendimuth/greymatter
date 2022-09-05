feca2396a08309c43daa5ead184fe75562272d9d
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-09-04 22:55:14 -0300

    add sanity experimental spaces

--- site/src/App.js
+++ site/src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeProvider from './context/HomeProvider';
 
 import './assets/css/bootstrap.min.css';
+import './assets/css/animate.min.css';
+import './assets/css/aos.css';
 import './assets/css/SegoeUI.css';
 import './css/application.css';
 import './css/community.css';
 import './assets/css/Footer-Dark-Multi-Column-icons.css';
 import './css/hero.css';
-import './assets/css/animate.min.css';
-import './assets/css/aos.css';
 import './css/styles.css';
 import './css/what-we-offer.css';
 import './css/what-we-look-for.css';
