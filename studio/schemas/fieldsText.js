d1c9e094a97fb8de9c85978d27f6fb58abb35571
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-09-20 22:44:09 -0300

    fix studio 404

--- studio/schemas/fieldsText.js
+++ studio/schemas/fieldsText.js
@@ -1,7 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+// eslint-disable max-len
 export default {
   default: {
     title: {
-      // eslint-disable-next-line max-len
       description: 'Preencha com o nome da página e o estado da versão. Se for preview, adicione hífen e a palavra preview. Ex. "Page name - Preview',
       title: 'Versão',
     },
