09f47521ad6afae1dbf9f8d34a9104e7055f8996
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-03-28 20:51:28 -0300

    fix: sanity query

--- site/src/utils/fetchContent.js
+++ site/src/utils/fetchContent.js
@@ -6,28 +6,28 @@ const fetchContent = async (doc, languageId) => {
       abbreviation, code, language, _id }`;
 
   const teamQuery = `*[_type == "team" 
-      && language._ref == "${languageId}" | order(_createdAt asc)[0] {
+      && language._ref == "${languageId}"] | order(_createdAt asc)[0] {
       pageTitle,
       'members':teamMembers.teamMembers[]->{name, alt, photoLg, position, linkedin, text, slug },
       }`;
 
   const communityQuery = `*[_type == "community" 
-      && language._ref == "${languageId}" | order(_createdAt asc)[0] {
+      && language._ref == "${languageId}"] | order(_createdAt asc)[0] {
       pageTitle, text, image, communityMembers,
       'members':communityMembers.communityMembers[]->{name, alt, photoLg, position, linkedin, text, slug }
       }`;
 
   const heroQuery = `*[_type == "hero" 
-      && language._ref == "${languageId}" | order(_createdAt asc)[0] {
+      && language._ref == "${languageId}"] | order(_createdAt asc)[0] {
       background, image, pageTitle, subTitle, text, title,
       "backgroundVideo": backgroundVideo.asset->url
       }`;
 
   const genericQuery = `*[_type == "${doc}" 
-      && language._ref == "${languageId}" | order(_createdAt asc)[0]`;
+      && language._ref == "${languageId}"] | order(_createdAt asc)[0]`;
 
   const howWeInvestQuery = `*[_type == "howWeInvest"
-      && language._ref == "${languageId}" | order(_createdAt asc)[0] {
+      && language._ref == "${languageId}"] | order(_createdAt asc)[0] {
       pageTitle, cards, firstCard,
       }`;
 
