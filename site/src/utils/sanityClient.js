59ad6a37d5083497439b9c8a6f9d8c0c9a78a12c
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-03-28 19:26:03 -0300

    refactor: dataset env variable

--- site/src/utils/sanityClient.js
+++ site/src/utils/sanityClient.js
@@ -1,9 +1,8 @@
 import sanityClient from '@sanity/client';
-import getSubdomain from './getSubdomain';
 
 export default sanityClient({
   projectId: process.env.REACT_APP_PROJECT_ID,
-  dataset: getSubdomain() || process.env.REACT_APP_PROJECT_DATASET,
+  dataset: process.env.REACT_APP_PROJECT_DATASET,
   apiVersion: '2021-10-21',
   useCdn: false,
 });
