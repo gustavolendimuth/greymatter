76c0803620419df83e54a5b13e41a79bf8b388af
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-10-18 09:44:47 -0300

    fix netlify deploy config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['localhost', 'cdn.sanity.io', 'flagcdn.com', 'source.unsplash.com'],
+     target: 'serverless',
   },
 }
 
