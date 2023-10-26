deb1d105a0391fb63acb14f4a91d792696c00c5b
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-10-26 19:24:40 -0300

    fix navbar z-index

--- frontend/app/_components/Navbar.tsx
+++ frontend/app/_components/Navbar.tsx
@@ -71,7 +71,7 @@ function Navbar({ locale, position, background }: NavbarProps) {
         <>
           <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-center md:h-24">
-              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+              <div className="absolute inset-y-0 left-0 z-20 flex items-center md:hidden">
                 {/* Mobile menu button */}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
